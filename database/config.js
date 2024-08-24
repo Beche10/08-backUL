727e447015bd256e5f51e32ca3f95e56fefeae7c
Augusto <villevip10@gmail.com>
2024-08-24 20:33:13 -0300

    adv

--- database/config.js
+++ database/config.js
@@ -2,6 +2,23 @@ import mongoose from 'mongoose';
 
 
 
-const dbConnection = async() => {
+export const dbConnection = async() => {
 
+try {
+
+    await mongoose.connect( process.env.MONGODB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+    })
+
+    
+} catch (error) {
+    console.log(error);
+    throw new Error('Error a la hora de iniciar la base de datos');
 }
+
+
+
+};
