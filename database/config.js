db38efc2e66980ebd05554ec17e1ef61a3fb1a9a
Augusto <villevip10@gmail.com>
2024-08-24 21:20:23 -0300

    adv

--- database/config.js
+++ database/config.js
@@ -5,10 +5,10 @@ export const dbConnection = async () => {
     await mongoose.connect(process.env.MONGODB_CNN, {    
     });
 
-    console.log("Database has been connected");
+    console.log("Database has been connected.");
 
     } catch (error) {
     console.log(error);
-    throw new Error("Error a la hora de iniciar la base de datos");
+    throw new Error("Error a la hora de iniciar la base de datos.");
   }
 };
