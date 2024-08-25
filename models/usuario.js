9ed3dad71ddf5603c2f4b908dadeff8b00f5d32f
Augusto <villevip10@gmail.com>
2024-08-24 21:09:05 -0300

    adv

---
+++ models/usuario.js
@@ -0,0 +1,16 @@
+import { Schema, model } from 'mongoose';
+
+export const UsuarioSchema = Schema({
+
+    nombre: {
+        type: String,
+        required: [true, "El nombre es requerido"]
+    },
+
+    correo: {
+        type: String,
+        required: [true, "El correo es requerido"],
+        unique: true
+    }
+
+});
