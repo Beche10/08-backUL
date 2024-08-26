418ba390d878ff78bd30e58ae25483f92f4f2785
Augusto <villevip10@gmail.com>
2024-08-26 11:58:16 -0300

    adv

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { check } from "express-validator";
 import { handleValidate } from "../middlewares/handleValidate.js";
-import { usuarioDelete, usuarioGet, usuarioPatch, usuarioPost, usuarioPut } from "../controllers/usuario.js";
-
+import {
+  usuarioDelete,
+  usuarioGet,
+  usuarioPatch,
+  usuarioPost,
+  usuarioPut,
+} from "../controllers/usuario.js";
+import { Role } from "../models/role.js";
 
 export const userRouter = Router();
 
@@ -10,12 +16,23 @@ userRouter.get("/", usuarioGet);
 
 userRouter.put("/:id", usuarioPut);
 
-userRouter.post("/",[
+userRouter.post(
+  "/",
+  [
     check("nombre", "El nombre es obligatorio.").not().isEmpty(),
-    check("password","El password debe tener al menos 6 carácateres.").isLength({ min: 6 }),
+    check(
+      "password",
+      "El password debe tener al menos 6 carácateres."
+    ).isLength({ min: 6 }),
     check("correo", "El correo no es válido.").isEmail(),
     //check("rol", "No es un rol válido.").isIn([ 'ADMIN_ROLE', 'USER_ROLE' ]),
-    handleValidate
+    check("rol").custom(async (rol = "") => {
+      const existeRol = await Role.findOne({ rol });
+      if (!existeRol) {
+        throw new Error(`El rol ${rol} no está registrado en la DB.`);
+      }
+    }),
+    handleValidate,
   ],
   usuarioPost
 );
