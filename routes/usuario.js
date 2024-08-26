d45819968df96a204ba8e5fb477cfe40475555a5
Augusto <villevip10@gmail.com>
2024-08-26 10:37:56 -0300

    adv

--- routes/usuario.js
+++ routes/usuario.js
@@ -9,6 +9,8 @@ userRouter.get("/", usuarioGet );
 userRouter.put("/:id", usuarioPut );
 
 userRouter.post("/", [
+   check('nombre', 'El nombre es obligatorio.').not().isEmpty(),
+   check('password', 'El password debe tener al menos 6 carácateres.').isLength({ min: 6 }),
    check('correo', 'El correo no es válido.').isEmail(), 
 ] ,usuarioPost );
 
