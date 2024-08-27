d918df755802c8c36bf2d6266fcd86ac0ceb8788
Augusto <villevip10@gmail.com>
2024-08-27 14:38:30 -0300

    adv

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,18 @@
-import { response, request } from 'express';
+import { response, request } from "express";
 
 export const login = (req = request, res = response) => {
+  const { correo, password } = req.body;
+
+  try {
     res.json({
-        msg: "Login OK"
+      msg: "Login OK",
+      correo,
+      password,
     });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "ERROR: Hable con el administrador.",
+    });
+  }
 };
