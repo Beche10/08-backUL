f580f4082e8ba49357198408363f0df47497faa1
Augusto <villevip10@gmail.com>
2024-08-27 19:40:58 -0300

    adv

--- middlewares/validateJWT.js
+++ middlewares/validateJWT.js
@@ -1,20 +1,21 @@
-import { request, response } from 'express';
-import jwt from 'jsonwebtoken';
+import { request, response } from "express";
+import jwt from "jsonwebtoken";
 
+export const validateJWT = (req = request, res = response, next) => {
+  const token = req.header("x-token");
 
-
-export const validateJWT = ( req = request, res = response, next ) => {
-
- const token = req.header('x-token');
-
- if(!token){
+  if (!token) {
     return res.status(401).json({
-        msg: 'No hay token en la petición.'
-    })
- }
+      msg: "No hay token en la petición.",
+    });
+  }
 
-
- console.log(token);
-
- next();
-}
+  try {
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      msg: "Token no válido.",
+    });
+  }
+};
