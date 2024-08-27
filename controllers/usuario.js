9db7c7d23477b257800d1248369143bce4197cd5
Augusto <villevip10@gmail.com>
2024-08-27 00:45:08 -0300

    adv

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -4,7 +4,7 @@ import bcryptjs from "bcryptjs";
 
 export const usuarioGet = async (req = request, res = response) => {
   const { limite = 5, desde = 0 } = req.query;
-  const usuarios = await Usuario.find()
+  const usuarios = await Usuario.find({ estado: true })
     .skip(Number(desde))
     .limit(Number(limite));
 
