6d68a3b436a315946d14612d179bd781b21c072e
Augusto <villevip10@gmail.com>
2024-08-28 17:36:25 -0300

    adv

--- routes/afiliado.js
+++ routes/afiliado.js
@@ -3,26 +3,26 @@ import { Router } from "express";
 export const afiliadoRouter = Router();
 
 // Obtener todos los afiliados - Privado
-afiliadoRouter.get("/", () => {
+afiliadoRouter.get("/", (req, res) => {
   res.json("get");
 });
 
 // Obtener afiliados especificos - Privado
-afiliadoRouter.get("/:id", () => {
+afiliadoRouter.get("/:id", (req, res) => {
   res.json("get - id");
 });
 
 // Crear afiliados - Publico
-afiliadoRouter.post("/", () => {
+afiliadoRouter.post("/", (req, res) => {
   res.json("post");
 });
 
 // Actualizar por ID - Admin
-afiliadoRouter.put("/:id", () => {
+afiliadoRouter.put("/:id", (req, res) => {
   res.json("put");
 });
 
 // Borrar un afiliado - Admin
-afiliadoRouter.delete("/:id", () => {
-    res.json("put");
-  });
+afiliadoRouter.delete("/:id", (req, res) => {
+  res.json("delete");
+});
