396d9af0e4b0800cbaf8d474035fcbd05cbd1e0b
Augusto <villevip10@gmail.com>
2024-08-31 13:32:18 -0300

    adv

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,38 +1,14 @@
 import { response } from "express";
-import path from "path";
-import { fileURLToPath } from "url"; // Necesario para calcular __dirname
-import { v4 as uuidv4 } from 'uuid';
+
 
 
 export const uploads = (req, res = response) => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
+ 
 
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
     res.status(400).json({ msg: "El contenedor no tiene ningun archivo." });
     return;
   }
 
-  const { archivo } = req.files;
-  const nombreCortado = archivo.name.split(".");
-  const extension = nombreCortado[nombreCortado.length - 1];
-
-  // Validar extension
-  const extensionesValidas = ["png", "jpg", "jpeg"];
-  if (!extensionesValidas.includes(extension)) {
-    return res.status(400).json({
-      msg: `La extension ${extension} no es permitida, ${extension}`,
-    });
-  }
-
-  const nombreTemp = uuidv4() + '.' + extension;
-  const uploadPath = path.join( __dirname, "../uploads/", nombreTemp );
-
-  archivo.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json({ err });
-    }
-
-    res.json({ msg: "File uploaded to" + uploadPath });
-  });
+  
 };
---
+++ helpers/uploadFile.js
@@ -0,0 +1,33 @@
+import path from "path";
+import { fileURLToPath } from "url"; // Necesario para calcular __dirname
+import { v4 as uuidv4 } from "uuid";
+
+export const uploadFile = (
+  files,
+  extensionesValidas = ["png", "jpg", "jpeg"]
+) => {
+  return new Promise((resolve, reject) => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const { archivo } = files;
+    const nombreCortado = archivo.name.split(".");
+    const extension = nombreCortado[nombreCortado.length - 1];
+
+    // Validar extension
+    if (!extensionesValidas.includes(extension)) {
+      reject(`La extension ${extension} no es permitida, ${extension}`);
+    }
+
+    const nombreTemp = uuidv4() + "." + extension;
+    const uploadPath = path.join(__dirname, "../uploads/", nombreTemp);
+
+    archivo.mv(uploadPath, (err) => {
+      if (err) {
+        return res.status(500).json({ err });
+      }
+
+      res.json({ msg: "File uploaded to" + uploadPath });
+    });
+  });
+};
