f1dd83e2ed81ff14c102caf08f8d397385a1dd02
Augusto <villevip10@gmail.com>
2024-08-31 01:36:20 -0300

    adv

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,12 +1,24 @@
 import { response } from "express";
 
 export const uploads = (req, res = response) => {
- 
-    console.log(req.files);
- 
- 
- 
-    res.json({
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+    res.status(400).json({ msg: "El contenedor no tiene ningun archivo." });
+    return;
+  }
+
+  const { archivo } = req.files;
+
+  uploadPath = __dirname + "/uploads/" + sampleFile.name;
+
+  sampleFile.mv(uploadPath, function (err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    res.send("File uploaded to " + uploadPath);
+  });
+
+  res.json({
     msg: "Soy el archivo subido",
   });
 };
