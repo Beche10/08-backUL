e403b896f573aa592cc81a1bd34d91b9c6a7dca8
Augusto <villevip10@gmail.com>
2024-09-02 15:42:12 -0300

    adv

--- middlewares/validateFile.js
+++ middlewares/validateFile.js
@@ -4,7 +4,7 @@ export const validateFile = (req, res = response, next) => {
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
     return res
       .status(400)
-      .json({ msg: "El contenedor no tiene ningun archivo. - validateFile" });
+      .json({ msg: "El contenedor no tiene ningun archivo." });
   }
 
   next();
