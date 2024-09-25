f6d111f9727db9dcb561ab3035a6398088fe8e79
Augusto <villevip10@gmail.com>
2024-09-25 11:38:22 -0300

    adv

--- controllers/afiliado.js
+++ controllers/afiliado.js
@@ -16,7 +16,7 @@ cloudinary.config({
 export const afiliadoGet = async (req = request, res = response) => {
   try {
     const afiliados = await Afiliado.find(); // Obtener todos los afiliados de la base de datos
-    const numAfiliados = await afiliados.countDocuments();
+    const numAfiliados = await Afiliado.countDocuments();
 
     res.json({
       msg: "Afiliados de DB:",
